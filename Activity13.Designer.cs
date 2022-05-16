e78bc3815f379d8720ca097473c985e2f9b6ed7a
tyson <87287439+tysonpatten@users.noreply.github.com>
2022-05-15 20:49:58 -0700

    Uploaded CS files and screenshots of program

---
+++ Activity13.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Activity13
+{
+    partial class Activity13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.RichTextBox();
+            this.box2 = new System.Windows.Forms.RichTextBox();
+            this.box3 = new System.Windows.Forms.RichTextBox();
+            this.box4 = new System.Windows.Forms.RichTextBox();
+            this.box5 = new System.Windows.Forms.RichTextBox();
+            this.box6 = new System.Windows.Forms.RichTextBox();
+            this.box7 = new System.Windows.Forms.RichTextBox();
+            this.box8 = new System.Windows.Forms.RichTextBox();
+            this.box9 = new System.Windows.Forms.RichTextBox();
+            this.winnerBox = new System.Windows.Forms.RichTextBox();
+            this.btn_newGame = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.box1.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box1.ForeColor = System.Drawing.Color.Black;
+            this.box1.Location = new System.Drawing.Point(62, 55);
+            this.box1.Name = "box1";
+            this.box1.ReadOnly = true;
+            this.box1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.box1.Size = new System.Drawing.Size(108, 103);
+            this.box1.TabIndex = 1;
+            this.box1.Text = "";
+            // 
+            // box2
+            // 
+            this.box2.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box2.Location = new System.Drawing.Point(199, 55);
+            this.box2.Name = "box2";
+            this.box2.ReadOnly = true;
+            this.box2.Size = new System.Drawing.Size(108, 103);
+            this.box2.TabIndex = 2;
+            this.box2.Text = "";
+            // 
+            // box3
+            // 
+            this.box3.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box3.Location = new System.Drawing.Point(336, 55);
+            this.box3.Name = "box3";
+            this.box3.ReadOnly = true;
+            this.box3.Size = new System.Drawing.Size(108, 103);
+            this.box3.TabIndex = 3;
+            this.box3.Text = "";
+            // 
+            // box4
+            // 
+            this.box4.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box4.Location = new System.Drawing.Point(62, 187);
+            this.box4.Name = "box4";
+            this.box4.ReadOnly = true;
+            this.box4.Size = new System.Drawing.Size(108, 103);
+            this.box4.TabIndex = 4;
+            this.box4.Text = "";
+            // 
+            // box5
+            // 
+            this.box5.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box5.Location = new System.Drawing.Point(199, 187);
+            this.box5.Name = "box5";
+            this.box5.ReadOnly = true;
+            this.box5.Size = new System.Drawing.Size(108, 103);
+            this.box5.TabIndex = 5;
+            this.box5.Text = "";
+            this.box5.TextChanged += new System.EventHandler(this.box5_TextChanged);
+            // 
+            // box6
+            // 
+            this.box6.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box6.Location = new System.Drawing.Point(336, 187);
+            this.box6.Name = "box6";
+            this.box6.ReadOnly = true;
+            this.box6.Size = new System.Drawing.Size(108, 103);
+            this.box6.TabIndex = 6;
+            this.box6.Text = "";
+            // 
+            // box7
+            // 
+            this.box7.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box7.Location = new System.Drawing.Point(62, 316);
+            this.box7.Name = "box7";
+            this.box7.ReadOnly = true;
+            this.box7.Size = new System.Drawing.Size(108, 103);
+            this.box7.TabIndex = 7;
+            this.box7.Text = "";
+            // 
+            // box8
+            // 
+            this.box8.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box8.Location = new System.Drawing.Point(199, 316);
+            this.box8.Name = "box8";
+            this.box8.ReadOnly = true;
+            this.box8.Size = new System.Drawing.Size(108, 103);
+            this.box8.TabIndex = 8;
+            this.box8.Text = "";
+            // 
+            // box9
+            // 
+            this.box9.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold);
+            this.box9.Location = new System.Drawing.Point(336, 316);
+            this.box9.Name = "box9";
+            this.box9.ReadOnly = true;
+            this.box9.Size = new System.Drawing.Size(108, 103);
+            this.box9.TabIndex = 9;
+            this.box9.Text = "";
+            // 
+            // winnerBox
+            // 
+            this.winnerBox.Location = new System.Drawing.Point(141, 451);
+            this.winnerBox.Name = "winnerBox";
+            this.winnerBox.ReadOnly = true;
+            this.winnerBox.Size = new System.Drawing.Size(223, 25);
+            this.winnerBox.TabIndex = 10;
+            this.winnerBox.Text = "";
+            // 
+            // btn_newGame
+            // 
+            this.btn_newGame.Location = new System.Drawing.Point(62, 514);
+            this.btn_newGame.Name = "btn_newGame";
+            this.btn_newGame.Size = new System.Drawing.Size(180, 35);
+            this.btn_newGame.TabIndex = 11;
+            this.btn_newGame.Text = "New Game";
+            this.btn_newGame.UseVisualStyleBackColor = true;
+            this.btn_newGame.Click += new System.EventHandler(this.btn_newGame_Click_1);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(264, 514);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(180, 35);
+            this.btn_Exit.TabIndex = 12;
+            this.btn_Exit.Text = "Exit Game";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click_1);
+            // 
+            // Activity13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 603);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_newGame);
+            this.Controls.Add(this.winnerBox);
+            this.Controls.Add(this.box9);
+            this.Controls.Add(this.box8);
+            this.Controls.Add(this.box7);
+            this.Controls.Add(this.box6);
+            this.Controls.Add(this.box5);
+            this.Controls.Add(this.box4);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Name = "Activity13";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox box1;
+        private System.Windows.Forms.RichTextBox box2;
+        private System.Windows.Forms.RichTextBox box3;
+        private System.Windows.Forms.RichTextBox box4;
+        private System.Windows.Forms.RichTextBox box5;
+        private System.Windows.Forms.RichTextBox box6;
+        private System.Windows.Forms.RichTextBox box7;
+        private System.Windows.Forms.RichTextBox box8;
+        private System.Windows.Forms.RichTextBox box9;
+        private System.Windows.Forms.RichTextBox winnerBox;
+        private System.Windows.Forms.Button btn_newGame;
+        private System.Windows.Forms.Button btn_Exit;
+    }
+}
+
---
+++ Activity13.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13
+{
+    public partial class Activity13 : Form
+    {
+        
+
+        // declare the game board array
+        int[,] gameBoard = new int[3, 3];
+
+        // variable for winner
+        string winner = "";
+
+        public Activity13()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_newGame_Click_1(object sender, EventArgs e)
+        {
+            // random number generator
+            Random rnd = new Random();
+
+            // loop through the game board array
+            for (int i = 0; i < 3; i++)  
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    // generate a random number between 0 and 1
+                    int rndNumber = rnd.Next(0, 2);
+
+                    // store the random number in the game board array
+                    gameBoard[i, j] = rndNumber;
+                }
+            }
+
+            // display the game board in the rich text boxes as O or X
+            if (gameBoard[0, 0] == 0)
+            {
+                box1.Text = "O";
+            }
+            else
+            {
+                box1.Text = "X";
+            }
+
+            if (gameBoard[0, 1] == 0)
+            {
+                box2.Text = "O";
+            }
+            else
+            {
+                box2.Text = "X";
+            }
+
+            if (gameBoard[0, 2] == 0)
+            {
+                box3.Text = "O";
+            }
+            else
+            {
+                box3.Text = "X";
+            }
+
+            if (gameBoard[1, 0] == 0)
+            {
+                box4.Text = "O";
+            }
+            else
+            {
+                box4.Text = "X";
+            }
+
+            if (gameBoard[1, 1] == 0)
+            {
+                box5.Text = "O";
+            }
+            else
+            {
+                box5.Text = "X";
+            }
+
+            if (gameBoard[1, 2] == 0)
+            {
+                box6.Text = "O";
+            }
+            else
+            {
+                box6.Text = "X";
+            }
+
+            if (gameBoard[2, 0] == 0)
+            {
+                box7.Text = "O";
+            }
+            else
+            {
+                box7.Text = "X";
+            }
+
+            if (gameBoard[2, 1] == 0)
+            {
+                box8.Text = "O";
+            }
+            else
+            {
+                box8.Text = "X";
+            }
+
+            if (gameBoard[2, 2] == 0)
+            {
+                box9.Text = "O";
+            }
+            else
+            {
+                box9.Text = "X";
+            }
+
+
+            // check for a winner
+            winner = CheckForWinner();
+
+            
+            if (winner == "")
+            {
+                winnerBox.Text = "It is a draw!";
+            }
+            else
+            {
+                winnerBox.Text = winner + " Wins!";
+            }
+
+        }
+
+        private string CheckForWinner()
+        {
+            // declare the winner
+            string winner = "";
+
+
+            // check to see if O won
+            if (gameBoard[0, 0] == 0 && gameBoard[0, 1] == 0 && gameBoard[0, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[1, 0] == 0 && gameBoard[1, 1] == 0 && gameBoard[1, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[2, 0] == 0 && gameBoard[2, 1] == 0 && gameBoard[2, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[0, 0] == 0 && gameBoard[1, 0] == 0 && gameBoard[2, 0] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[0, 1] == 0 && gameBoard[1, 1] == 0 && gameBoard[2, 1] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[0, 2] == 0 && gameBoard[1, 2] == 0 && gameBoard[2, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[0, 0] == 0 && gameBoard[1, 1] == 0 && gameBoard[2, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gameBoard[0, 2] == 0 && gameBoard[1, 1] == 0 && gameBoard[2, 0] == 0)
+            {
+                winner = "O";
+            }
+
+            // check to see if X won
+            if (gameBoard[0, 0] == 1 && gameBoard[0, 1] == 1 && gameBoard[0, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[1, 0] == 1 && gameBoard[1, 1] == 1 && gameBoard[1, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[2, 0] == 1 && gameBoard[2, 1] == 1 && gameBoard[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[0, 0] == 1 && gameBoard[1, 0] == 1 && gameBoard[2, 0] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[0, 1] == 1 && gameBoard[1, 1] == 1 && gameBoard[2, 1] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[0, 2] == 1 && gameBoard[1, 2] == 1 && gameBoard[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[0, 0] == 1 && gameBoard[1, 1] == 1 && gameBoard[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gameBoard[0, 2] == 1 && gameBoard[1, 1] == 1 && gameBoard[2, 0] == 1)
+            {
+                winner = "X";
+            }
+            
+
+            // return the winner
+            return winner;
+        }
+        private void btn_Exit_Click_1(object sender, EventArgs e)
+        {
+            // close app
+            this.Close();
+        }
+
+        
+
+
+
+
+        
+
+        private void box5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
